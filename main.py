79f62f8f72f4a18d0a6fde1915d993ac441dc104
14thDovePin <therlaf0409@gmail.com>
2023-11-05 12:13:05 +0800

    Listed all `Polygon`s inside main.
    - Printed the length of the list.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from shapely import Polygon, MultiPolygon
+
 import geopandas as gp
 
 
 LAND_DATASET_DIR = ".\\assets\\ne_10m_land.shp"
 MINOR_ISLAND_DATASET_DIR = ".\\assets\\ne_10m_minor_islands.shp"
 
-def main():
+def main(include_minor=True):
     # Land Dataset
     lds = gp.read_file(LAND_DATASET_DIR)
     # Minor Islands Dataset
@@ -12,10 +14,25 @@ def main():
 
     lgs = lds['geometry']  # Land GeoSereies
     migs = mids['geometry']  # Minor Islands GeoSeries
-    
-    print(lgs)
-    print('====')
-    print(migs)
+
+    polygons = []
+
+    for polygon in lgs:
+        if type(polygon) is Polygon:
+            polygons.append(polygon)
+        else:
+            for i in polygon.geoms:
+                polygons.append(i)
+
+    if include_minor:
+        for polygon in migs:
+            if type(polygon) is Polygon:
+                polygons.append(polygon)
+            else:
+                for i in polygon.geoms:
+                    polygons.append(i)
+
+    print(len(polygons))
 
 
 if __name__ == '__main__':
