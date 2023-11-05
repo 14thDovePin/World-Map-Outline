1de6636830a74cf185ef14210a941b21830a0b9e
14thDovePin <therlaf0409@gmail.com>
2023-11-05 12:05:18 +0800

    Opened the shape assets.
    - Printed the geoseries of both Land & Minor Islands datasets.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import geopandas as gp
+
+
+LAND_DATASET_DIR = ".\\assets\\ne_10m_land.shp"
+MINOR_ISLAND_DATASET_DIR = ".\\assets\\ne_10m_minor_islands.shp"
+
 def main():
-    pass
+    # Land Dataset
+    lds = gp.read_file(LAND_DATASET_DIR)
+    # Minor Islands Dataset
+    mids = gp.read_file(MINOR_ISLAND_DATASET_DIR)
+
+    lgs = lds['geometry']  # Land GeoSereies
+    migs = mids['geometry']  # Minor Islands GeoSeries
+    
+    print(lgs)
+    print('====')
+    print(migs)
 
 
 if __name__ == '__main__':
